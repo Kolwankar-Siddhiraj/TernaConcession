99dd67c7e2fc995a5f4515b13bda97c6eaa622ff
Kolwankar-Siddhiraj <siddhirajk77@gmail.com>
2023-08-15 19:20:04 +0530

    added urls file

--- concession_app/views.py
+++ concession_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+
+
+def index(request):
+
+    return render(request, '')
+
+
+def login(request):
+
+    return render(request, '')
+
+
+def register(request):
+
+    return render(request, '')
+
+
