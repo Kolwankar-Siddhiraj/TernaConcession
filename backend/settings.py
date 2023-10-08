c7886923a9bdcb48154abb0f9f438fe7c610724e
Kolwankar-Siddhiraj <siddhirajk77@gmail.com>
2023-10-08 13:50:39 +0530

    added login signup verify-user personal-details

--- backend/settings.py
+++ backend/settings.py
@@ -174,5 +174,18 @@ STATICFILES_DIRS = [
 
 
 
+# mail server configuration
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = True
+# EMAIL_HOST = os.getenv("EMAIL_HOST")
+# EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+
 
 
--- concession_app/urls.py
+++ concession_app/urls.py
@@ -4,7 +4,16 @@ urlpatterns = [
     path('', index, name="index"),
     path('login', LoginView, name="login"),
     path('register', RegisterView, name="register"),
-    
+    path('<str:action>/verify/<str:token>', VerifyUserView, name="verify-user"),
+    path('verified', Verified, name="verified"),
+
+
+    # student
+    path('personal-details', PersonalDetailsView, name="personal-details"),
+    path('college-details', CollegeDetailsView, name="college-details"),
+    path('train-details', TrainDetailsView, name="train-details"),
+    path('ticket-details', TicketDetailsView, name="train-details"),
+
     
     # test
     path('test', test, name="test"),
